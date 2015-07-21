a1c34f107161193fe7c34b317f6bd5d977ced6f1
Elviro Rocca <elviro.rocca@facile.it>
2015-07-21 10:00:33 +0200

    Initial Commit

---
+++ ErrorClient/ErrorClient.h
@@ -0,0 +1,13 @@
+//
+//  ErrorClient.h
+//  ErrorClient
+//
+//  Created by Elviro Rocca on 21/07/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ErrorClient : NSObject
+
+@end
---
+++ ErrorClient/ErrorClient.m
@@ -0,0 +1,13 @@
+//
+//  ErrorClient.m
+//  ErrorClient
+//
+//  Created by Elviro Rocca on 21/07/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import "ErrorClient.h"
+
+@implementation ErrorClient
+
+@end
